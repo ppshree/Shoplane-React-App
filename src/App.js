f2a23e718e54c3d05f305afe2b330049008ce99b
ppshree <ppankajshree@gmail.com>
2021-08-16 17:20:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopbarPage from "./Components/Topbar/TopbarPage";
+import FooterPage from "./Components/Footer/FooterPage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
+import ProductDetailsPage from "./Components/ProductDetails/ProductDetailsPage";
+import CartPage from "./Components/CartPage/CartPage";
+import ThankYou from "./Components/ThankYou Page/ThankYou";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <TopbarPage />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/product/:id" component={ProductDetailsPage}></Route>
+          <Route path="/cart" component={CartPage}></Route>
+          <Route path="/thank-you" component={ThankYou}></Route>
+        </Switch>
+        <FooterPage />
+      </BrowserRouter>
     </div>
   );
 }
